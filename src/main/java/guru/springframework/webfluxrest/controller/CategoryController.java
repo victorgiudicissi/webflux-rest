dc839040a8941ad2bf64a0fe6546c488e7288f22
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-08 16:10:47 -0300

    Adicionado endpoint para encontrar categoria por id

--- src/main/java/guru/springframework/webfluxrest/controller/CategoryController.java
+++ src/main/java/guru/springframework/webfluxrest/controller/CategoryController.java
@@ -3,9 +3,11 @@ package guru.springframework.webfluxrest.controller;
 import guru.springframework.webfluxrest.domain.Category;
 import guru.springframework.webfluxrest.repository.CategoryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1/category")
@@ -20,5 +22,10 @@ public class CategoryController {
     @GetMapping
     Flux<Category> listAll() {
         return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Mono<Category> findById(@PathVariable String id) {
+        return categoryRepository.findById(id);
     }
 }
