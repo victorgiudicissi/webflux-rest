d46560ce18e4637c61228ff428cd5573fdab806c
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-08 15:09:41 -0300

    Commit inicial

---
+++ src/main/java/guru/springframework/webfluxrest/WebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/webfluxrest/WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
