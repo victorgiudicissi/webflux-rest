4700fa065d847a83057d8739979099a381808dbf
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-08 16:03:18 -0300

    Criado controller para a categoria

--- src/main/java/guru/springframework/webfluxrest/controller/CategoryController.java
+++ src/main/java/guru/springframework/webfluxrest/controller/CategoryController.java
@@ -1,4 +1,24 @@
 package guru.springframework.webfluxrest.controller;
 
+import guru.springframework.webfluxrest.domain.Category;
+import guru.springframework.webfluxrest.repository.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api/v1/category")
 public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    Flux<Category> listAll() {
+        return categoryRepository.findAll();
+    }
 }
